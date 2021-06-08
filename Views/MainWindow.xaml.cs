096f2dfd5046e01c9aaf8647beb2d70b631cdee1
Michelle <jaramayo184@gmail.com>
2021-06-08 23:18:23 +0100

    cansada

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Garbage_Organizer
 {
@@ -23,6 +12,24 @@ namespace Garbage_Organizer
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            bool? response = openFileDialog.ShowDialog();
+            if (response == true)
+            {
+                 string filePath = openFileDialog.FileName;
+                string fileName = Path.GetDirectoryName(filePath);
+            }
+
+            
         }
     }
 }
